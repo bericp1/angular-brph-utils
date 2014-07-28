53ef8c8c18aad69961ca5a54e0f251f52f79d736
Brandon Phillips <bericp1@gmail.com>
2014-07-27 20:47:57 -0600

    added brph-float-height directove

--- index.js
+++ index.js
@@ -11,6 +11,29 @@
           });
         }
       };
-    });
+    })
+    .directive('brphFloatHeight', ['$rootScope', function($rootScope){
+      var thisHeight = function(){
+        return $(this).outerHeight(true);
+      };
+      var floatFilter = function(){
+        var $this = $(this);
+        return $this.css('float') === 'left' || $this.css('float') === 'right';
+      };
+      var adjustHeight = function($elem){
+        $elem.height(function() {
+          return Math.max.apply(Math, $(this).children().filter(floatFilter).map(thisHeight));
+        });
+      };
+      return {
+        restrict : 'A',
+        link: function(scope, $elem){
+          adjustHeight($elem);
+          $rootScope.$on('$stateChangeSuccess', function(){
+            adjustHeight($elem);
+          });
+        }
+      };
+    }]);
 
 })(angular);
